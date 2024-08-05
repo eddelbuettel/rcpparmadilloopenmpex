e1250ded4d508e7500d6a5d51a266ec55cb6703b
Dirk Eddelbuettel <edd@debian.org>
2024-08-05 08:57:23 -0500

    Add spdl to show we can use both, and use of stopwatch

--- src/example.cpp
+++ src/example.cpp
@@ -1,7 +1,7 @@
-#define ARMA_64BIT_WORD
 #include <RcppArmadillo.h>
+#include <spdl.h>               		// has to come after RcppArmadillo as former checks for Rcpp
 
-using namespace arma;
+//using namespace arma;
 
 //' Example Function to Illustrate OpenMP Use
 //'
@@ -11,6 +11,12 @@ using namespace arma;
 //' time'. However, when environment variable 'SHOWME' is set to 'yes'
 //' we throttle and no warning appears.
 //'
+//' For the example size used here, we see two threads (a little) faster
+//' in elapsed 'wall time' than one. For larger sizes more threads gain more.
+//'
+//' This is simply not the strongest example of OpenMP benefits but simply
+//' an 'existence proof' for a working OpenMP configuration.
+//'
 //' @examples
 //' if (Sys.getenv("SHOWME", "") == "yes")
 //'    set_number_of_omp_threads(1)
@@ -18,19 +24,24 @@ using namespace arma;
 //'
 // [[Rcpp::export]]
 void openmp_example() {
+    spdlog::stopwatch sw;
     // generate sparse matrix
-    sp_mat A = sprandu<sp_mat>(1500, 1500, 0.1);
-    sp_mat B = A.t()*A;
+    arma::sp_mat A = arma::sprandu<arma::sp_mat>(1500, 1500, 0.05);
+    arma::sp_mat B = A.t()*A;
 
-    vec eigval;
-    mat eigvec;
+    arma::vec eigval;
+    arma::mat eigvec;
 
-    eigs_sym(eigval, eigvec, B, 5);  // find 5 eigenvalues/eigenvectors
+    arma::eigs_sym(eigval, eigvec, B, 5);  // find 5 eigenvalues/eigenvectors
+    spdl::warn("[openmp_example] time elapsed {}", sw);
 
-    eigs_opts opts;
+    arma::eigs_opts opts;
     opts.maxiter = 10000;            // increase max iterations to 10000
 
-    eigs_sym(eigval, eigvec, B, 5, "lm", opts);
+    arma::eigs_sym(eigval, eigvec, B, 5, "lm", opts);
+    spdl::warn("[openmp_example] time elapsed {}", sw);
+    //eigval.print("eigval");
+    //eigvec.print("eigvec");
 }
 
 //' Report Maximum Number of Threads
@@ -38,6 +49,7 @@ void openmp_example() {
 // [[Rcpp::export]]
 int get_number_of_omp_threads() {
 #ifdef _OPENMP
+    spdl::warn("[get_number_of_omp_threads] seeing {}", omp_get_max_threads());
     return omp_get_max_threads();
 #else
     return 1;
